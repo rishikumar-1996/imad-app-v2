18ba26695fb83de5100e51db35935563d06aa721
rishikumar-1996 <rishi13317@ece.ssn.edu.in>
2017-02-26 20:56:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,12 @@
-console.log('Loaded!');
+var button = document.getElementById('counter');
+var counter=0;
+button.onclick = function() {
+  
+  
+  
+  
+  
+  counter = counter+1;
+  var span = document.getElementById('count');
+  span.innerHTML = counter.toString();
+}; 
