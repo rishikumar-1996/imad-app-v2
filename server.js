9e73a8870a95b857cb02ae74d37afeaf92d4f2b1
rishikumar-1996 <rishi13317@ece.ssn.edu.in>
2017-02-26 20:44:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ app.get('/', function (req, res) {
 
 var counter=0;
 app.get('/counter', function(req, res) {
-   counter = counter+1;
-   res.send(counter.tostring());
+   counter = counter + 1;
+   res.send(counter.toString());
 });
 
 
