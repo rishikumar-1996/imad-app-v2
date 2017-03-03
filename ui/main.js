11b3209efb3a4d8ac6be8635de38e4cb154ca0ca
rishikumar-1996 <rishi13317@ece.ssn.edu.in>
2017-03-03 10:56:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,12 +4,11 @@ button.onclick = function()
 {
 var request = new XMLHttpRequest(); 
 request.onreadystatechange = function(){
-    if(request.readyState === XMLHttpRequest.DONE){
+    if(request.readyState === XMLHttpRequest.DONE)
     if(request.status === 200){
         var counter = request.responseText;
         var span = document.getElementById('count');
         span.innerHTML = counter.toString();
-    }
     }
 };
   
