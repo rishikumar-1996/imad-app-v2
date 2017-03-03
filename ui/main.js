b5bdeefc61557392c7de219748734fe8f7ccd726
rishikumar-1996 <rishi13317@ece.ssn.edu.in>
2017-03-03 10:43:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,19 @@
 var button = document.getElementById('counter');
-var counter=3;
+var counter=0;
 button.onclick = function() 
 {
 var request = new XMLHttpRequest(); 
 request.onreadystatechange = function(){
-    if(request.readyState === XMLHttpRequest.DONE)
+    if(request.readyState === XMLHttpRequest.DONE){
     if(request.status === 200){
-        var counter = this.responseText;
+        var counter = request.responseText;
         var span = document.getElementById('count');
         span.innerHTML = counter.toString();
+    }
     }
 };
   
   
-  request.open('GET','/counter', true);
+  request.open('GET','http://rishikumar-1996.imad.hasura-app.io/counter', true);
   request.send(null);
 }; 
