f857ca1fb330bb60fa893e634cedebf915537877
rishikumar-1996 <rishi13317@ece.ssn.edu.in>
2017-03-04 19:59:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
 
+var config = {
+  host: 'db.imad.hasura-app.io',
+  user: 'rishikumar-1996',
+  database: 'rishikumar-1996',
+  port: 5432,
+  password: process.env.DB_PASSWORD
+};
 var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+var pool = new Pool(config);
+app.get('/test-db', function (req, res) {
+pool.query('SELECT * FROM test', function(err, result){
+    if (err){
+        res.status(500).send(err.toString());
+        } else{
+            res.send(JSON.stringify(result));
+        }
+});
+});
+
 
 var counter=0;
 app.get('/counter', function(req, res) {
